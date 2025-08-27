db591a665660867f5dd630c219befe93bf1d2f46
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2025-08-27 14:20:10 +0530

    Matrix Rotation

---
+++ test.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    void reverseRows(vector<vector<int>>&matrix,int n,int i){
+             int ptr1=0,ptr2=n-1;
+             while(ptr1<ptr2){               
+                    swap(matrix[i][ptr1],matrix[i][ptr2]);
+                    ptr1++;ptr2--;
+             }
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        if(n==1)return;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){               
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+           reverseRows(matrix,n,i);
+        }
+        
+    }
+};
+int main(){
+ int n;
+ cin>>n;
+ vector<vector<int>>matrix(n,vector<int>(n));
+ 
+ for(int i=0;i<n;i++){
+  for(int j=0;j<n;j++){
+   cin>>matrix[i][j];
+  }
+ }
+ Solution Obj;
+ Obj.rotate(matrix);
+ cout<<"Rotated matrix:"<<endl;
+ for(int i=0;i<n;i++){
+  for(int j=0;j<n;j++){
+   cout<<matrix[i][j]<<" ";
+  }
+  cout<<"\n";
+ }
+ return 0;
+}
