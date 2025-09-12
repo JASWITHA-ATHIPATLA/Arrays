f8f0c4f0216920a5dac8a50719e5cd0e19263f7d
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2025-09-12 22:56:31 +0530

    Added uniqueFirstChar solution

---
+++ uniqueFirstChar.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n=s.size();
+        map<char,int>mapp;
+        for(int i=0;i<n;i++){
+            mapp[s[i]]++;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(mapp[s[i]]==1)return i;
+        }
+        return -1;
+    }
+
+};
+int main(){
+ string s;
+ cin>>s;
+ Solution obj;
+ int ans=obj.firstUniqChar(s);
+ cout<<"Unique first character"<<ans;
+ return 0;
+}
