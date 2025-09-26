464679fd3975ea7ba5bcc02103afbde736090f37
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2025-09-26 22:43:43 +0530

    leetcode prob:3683

--- earliestTimeToFinishTask.cpp
+++ earliestTimeToFinishTask.cpp
@@ -1,3 +1,21 @@
+// leetcode problem-3683-Earliest Time to Finish One Task
+
+// You are given a 2D integer array tasks where tasks[i] = [si, ti].
+
+// Each [si, ti] in tasks represents a task with start time si that takes ti units of time to finish.
+
+// Return the earliest time at which at least one task is finished.
+// Example 1:
+
+// Input: tasks = [[1,6],[2,3]]
+
+// Output: 5
+
+// Explanation:
+
+// The first task starts at time t = 1 and finishes at time 1 + 6 = 7.
+// The second task finishes at time 2 + 3 = 5. You can finish one task at time 5.
+
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
